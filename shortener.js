6c882a87ad8967aace4c4498ff7ff97a4ce9f834
Greg MacGregor <gmacgregor@gmail.com>
2013-11-27 23:56:31 -0500

    updates

--- shortener.js
+++ shortener.js
@@ -1,37 +1,58 @@
 (function( exports, $ ) {
 
-  var $form = $('form').first(),
-    $url = $form.find('#url'),
-    $slug = $form.find('#slug'),
-    $msg = $('.js-messaging').first();
+var store = exports.sessionStorage,
+  $form = $('form').first(),
+  $url = $form.find('#url'),
+  $slug = $form.find('#slug'),
+  $msg = $('.js-messaging').first(),
+  $code = $('code').first(),
+  api = 'http://tgam.ca/shorten/json/',
+  path = '';
 
-  $form.on('submit', shorten.bind( $form ));
+$form.on( 'submit', shorten.bind( $form ) );
+$code.on( 'click', edit );
 
-  function shorten( e ) {
-    e.preventDefault();
-    var url = $.trim($url.val()),
-        slug = $.trim($slug.val());
+function cleanUrl( url ) {
+  return $.trim( url.split('?')[0].split('#')[0] ) + path;
+}
 
-    if ( !url ) {
-      return;
-    }
+function edit( evt ) {
+  var $thing = $(evt.currentTarget);
+}
 
-    $.ajax({
-      dataType: 'jsonp',
-      url: 'http://tgam.ca/shorten/json/',
-      data: {
-        url: url,
-        slug: slug
-      }
-    })
+function shorten( e ) {
+  e.preventDefault();
+  var url = cleanUrl( $url.val() ),
+      slug = $.trim( $slug.val() ),
+      d = { url: url, slug: slug };
+
+  if ( !url ) {
+    return;
+  }
+  console.log(url);
+  $.ajax( { dataType: 'jsonp', url: api, data: d } )
     .done( respond )
-    .fail( function() {
-      alert('Dammit!');
-    });
-  }
+    .fail( facepalm );
+}
 
-  function respond( resp ) {
-    $msg.html( resp.result.short_url );
+function respond( resp ) {
+  var d = resp.result;
+  if ( d.success === 'false' ) {
+    return facepalm();
   }
+  return party( d.short_url );
+}
+
+function facepalm() {
+ $msg
+  .addClass( 'alert alert-error' )
+  .html( '<p class="has-error">There was an error shortening that url. Please try again.</p>' );
+}
+
+function party( url ) {
+  $url.val('');
+  $slug.val('');
+  $msg.addClass('alert alert-success').html('<p>Success! <strong>' + url + '</strong></p>');
+}
 
 })( window, window.jQuery);
